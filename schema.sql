80454119ef5632dae191fc6629a96f5e74bb2e01
KevinDBrian <kevindouglasbrian@gmail.com>
2022-02-10 06:20:31 -0600

    Added retirement_info.csv and updated queries

--- schema.sql
+++ schema.sql
@@ -50,3 +50,8 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 );
 -- Making sure tables were created correctly
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
