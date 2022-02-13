27813898b422cbd04b8a66a186c7f01c0f2727ea
KevinDBrian <kevindouglasbrian@gmail.com>
2022-02-13 16:18:25 -0600

    Challenge complete

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -158,3 +158,84 @@ GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
 SELECT * FROM retirements_per_dept
+
+--Creating additional lists
+SELECT * FROM  salaries
+ORDER BY to_date DESC;
+
+SELECT	e.emp_no, 
+		e.first_name, 
+		e.last_name,
+		e.gender,
+		s.salary,
+		de.to_date
+INTO emp_info
+FROM employees as e
+	INNER JOIN salaries as s
+		ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+-- INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+--List 3: Department Retiress
+SELECT	ce.emp_no,
+		ce.first_name,
+		ce.last_name,
+		d.dept_name
+--INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no);
+
+--Skill Drill 7.3.6 #1 Sales Emp Table
+SELECT	ri.emp_no,
+		ri.first_name,
+		ri.last_name,
+		d.dept_name
+-- INTO retire_sales_emp
+FROM dept_emp as de
+	INNER JOIN retirement_info as ri
+		ON (de.emp_no = ri.emp_no)
+	INNER JOIN departments as d
+		ON (d.dept_no = de.dept_no)
+WHERE d.dept_name IN ('Sales')
+
+
+--Skill Drill 7.3.6 #2 Sales and Dev Emp Table
+SELECT	ri.emp_no,
+		ri.first_name,
+		ri.last_name,
+		d.dept_name
+INTO mentor_list
+FROM dept_emp as de
+	INNER JOIN retirement_info as ri
+		ON (de.emp_no = ri.emp_no)
+	INNER JOIN departments as d
+		ON (d.dept_no = de.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
+
+SELECT * FROM mentor_list
+
+
+
+
+
